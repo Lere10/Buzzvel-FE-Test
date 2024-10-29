c4bcbc9f0aa4d199ccbed5492d0624bc0f5230f2
Lere <leredsb10@gmail.com>
2024-10-29 00:01:12 -0300

    carrossel

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,11 @@
 export default class Card {
-  constructor(info, templateSelector) {
+  constructor(info, templateSelector, index) {
     this._name = info.author;
     this._thumb = info.thumb;
     this._text = info.text;
     this._kwh = info.kwh;
     this._template = templateSelector;
+    this._index = index;
   }
 
   _getTemplate() {
@@ -15,8 +16,13 @@ export default class Card {
     return cardTemplate;
   }
 
-  generateCard() {
+  getIndex() {
+    return this._index;
+  }
+
+  generateCard(index) {
     this._element = this._getTemplate();
+
     this._element.querySelector(".quotes__cardtext").textContent = this._text;
     this._element.querySelector(".quotes__cardauthor-img").src = this._thumb;
     this._element.querySelector(".quotes__cardauthor-infos-name").textContent =
@@ -24,6 +30,12 @@ export default class Card {
     this._element.querySelector(
       ".quotes__cardauthor-infos-subname"
     ).textContent = this._kwh;
+
+    this._index !== null &&
+      this._element.setAttribute("id", `quote_${this._index}`);
+
+    this._index === index &&
+      this._element.classList.add("quotes__cardbox_focus");
 
     return this._element;
   }
